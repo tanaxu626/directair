7939ce47bc9e22c28bb1704b35b10e948980691d
brendanxu <brendanxu@users.noreply.github.com>
2026-08-25 20:56:18 -0400

    feat: release DirectAir MiniProgram v1.34 - comprehensive 250+ civil aviation airport database (Dali DLU, Lijiang LJG, Xishuangbanna JHG, etc.)

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,38 +1,137 @@
 // pages/index/index.js
-// 直航 DirectAir 100% 原生全屏流体城市搜索与防遮挡版 (v1.31)
+// 直航 DirectAir 100% 原生全量全国 250+ 机场与全球枢纽数据库版 (v1.34)
 
-const MOCK_DOMESTIC_HUBS = [
-  { code: 'PEK', city: '北京', airport: '首都国际 T2/T3', note: '国航/海航基地 · 距市中心25km', keywords: 'beijing shoudu pek bjs bj' },
-  { code: 'PKX', city: '北京', airport: '大兴国际', note: '东航/南航基地 · 高铁20分钟抵京', keywords: 'beijing daxing pkx dx' },
-  { code: 'SHA', city: '上海', airport: '虹桥国际 T2', note: '京沪商务核心 · 距市中心13km', keywords: 'shanghai hongqiao sha hq sh' },
-  { code: 'PVG', city: '上海', airport: '浦东国际 T1/T2', note: '国际与远程枢纽 · 距市中心30km', keywords: 'shanghai pudong pvg pd' },
-  { code: 'CAN', city: '广州', airport: '白云国际 T1/T2', note: '南航主基地 · 华南第一枢纽', keywords: 'guangzhou baiyun can by gz' },
-  { code: 'SZX', city: '深圳', airport: '宝安国际 T3', note: '深航主基地 · 粤港澳大湾区核心', keywords: 'shenzhen baoan szx ba sz' },
-  { code: 'CTU', city: '成都', airport: '双流国际 T2', note: '市区老牌枢纽 · 距春熙路16km', keywords: 'chengdu shuangliu ctu sl cd' },
-  { code: 'TFU', city: '成都', airport: '天府国际 T2', note: '西南超大超级枢纽 · 18号线直达', keywords: 'chengdu tianfu tfu tf' },
-  { code: 'HGH', city: '杭州', airport: '萧山国际 T3/T4', note: '长三角南翼枢纽 · 19号线快线', keywords: 'hangzhou xiaoshan hgh xs hz' },
-  { code: 'XIY', city: '西安', airport: '咸阳国际 T3/T5', note: '西北核心航空枢纽', keywords: 'xian xianyang xiy xy xa' },
-  { code: 'CKG', city: '重庆', airport: '江北国际 T3', note: '成渝双城经济圈核心', keywords: 'chongqing jiangbei ckg jb cq' },
-  { code: 'KMG', city: '昆明', airport: '长水国际', note: '东航云南基地 · 面向南亚门户', keywords: 'kunming changshui kmg cs km' },
-  { code: 'WUH', city: '武汉', airport: '天河国际 T3', note: '九省通衢中部大枢纽', keywords: 'wuhan tianhe wuh th wh' },
-  { code: 'NKG', city: '南京', airport: '禄口国际 T2', note: '江苏主枢纽 · S1号线直通', keywords: 'nanjing lukou nkg lk nj' },
-  { code: 'XMN', city: '厦门', airport: '高崎国际 T3/T4', note: '厦航主基地 · 滨海花园枢纽', keywords: 'xiamen gaoqi xmn gq xm' },
-  { code: 'SYX', city: '三亚', airport: '凤凰国际 T1/T2', note: '热带度假航线核心', keywords: 'sanya fenghuang syx fh sy' },
-  { code: 'HAK', city: '海口', airport: '美兰国际 T2', note: '海南自贸港核心门户', keywords: 'haikou meilan hak ml hk' },
-  { code: 'TAO', city: '青岛', airport: '胶东国际', note: '胶东半岛综合枢纽', keywords: 'qingdao jiaodong tao jd qd' }
-];
+// 涵盖全国所有省份主要/支线机场 + 国际超大都市
+const COMPREHENSIVE_AIRPORTS_DATABASE = [
+  // 1. 云南省航点 (大理/丽江/版纳/腾冲/香格里拉等)
+  { code: 'DLU', city: '大理', airport: '凤仪机场 (荒草坝)', note: '苍山洱海度假胜地 · 距古城20km', keywords: 'dali fengyi huangcaoba dlu dl yn yunnan 大理 凤仪 荒草坝' },
+  { code: 'LJG', city: '丽江', airport: '三义国际机场', note: '玉龙雪山门户 · 距古城28km', keywords: 'lijiang sanyi ljg sanyiguoji yn yunnan 丽江 三义' },
+  { code: 'KMG', city: '昆明', airport: '长水国际 T1/T2', note: '东航云南基地 · 面向南亚门户', keywords: 'kunming changshui kmg cs km yn 昆明 长水' },
+  { code: 'JHG', city: '西双版纳', airport: '嘎洒国际机场', note: '热带雨林傣乡门户 · 距景洪市中心5km', keywords: 'xishuangbanna gasa jhg bn banna 西双版纳 嘎洒 版纳' },
+  { code: 'DIG', city: '香格里拉', airport: '迪庆香格里拉机场', note: '雪域高原秘境 · 距独克宗古城5km', keywords: 'xianggelila diqing dig xgll 香格里拉 迪庆' },
+  { code: 'TCZ', city: '腾冲', airport: '驼峰机场', note: '极边第一城 · 温泉地热胜地', keywords: 'tengchong tuofeng tcz tf tc 腾冲 驼峰' },
+  { code: 'LUM', city: '芒市', airport: '德宏芒市机场', note: '孔雀之乡 · 中缅边境口岸', keywords: 'mangshi dehong lum ms 芒市 德宏' },
+  { code: 'BSD', city: '保山', airport: '云瑞机场', note: '滇西枢纽门户', keywords: 'baoshan yunrui bsd bs 保山 云瑞' },
+  { code: 'SYM', city: '普洱', airport: '思茅机场', note: '中国茶城 · 普洱茶原产地', keywords: 'puer simao sym pe 普洱 思茅' },
+  { code: 'ZAT', city: '昭通', airport: '昭通机场', note: '滇东北高原航点', keywords: 'zhaotong zat zt 昭通' },
+  { code: 'LNJ', city: '临沧', airport: '博尚机场', note: '世界佤乡 · 恒春之都', keywords: 'lincang boshang lnj lc 临沧' },
+  { code: 'JMJ', city: '澜沧', airport: '景迈机场', note: '景迈山古茶林文化景观', keywords: 'lancang jingmai jmj 澜沧 景迈' },
+  { code: 'CWJ', city: '沧源', airport: '佤山机场', note: '阿佤山水 · 秘境沧源', keywords: 'cangyuan washan cwj cy 沧源 佤山' },
 
-const MOCK_INTERNATIONAL_HUBS = [
-  { code: 'HKG', city: '香港', airport: '香港国际 T1', note: '国泰航空超级主基地 · 离境退税¥120', keywords: 'hongkong xianggang hkg hk' },
-  { code: 'MFM', city: '澳门', airport: '澳门国际', note: '珠三角一国两制便捷口岸', keywords: 'macau aomen mfm am' },
-  { code: 'TPE', city: '台北', airport: '桃园国际 T2', note: '华航/长荣主基地', keywords: 'taipei taoyuan tpe tb' },
-  { code: 'TYO', city: '东京', airport: '羽田 HND / 成田 NRT', note: '日航/全日空超级枢纽', keywords: 'tokyo dongjing haneda narita tyo hnd nrt' },
-  { code: 'OSA', city: '大阪', airport: '关西国际 KIX', note: '关西空港人工岛枢纽', keywords: 'osaka daban kansai osa kix' },
-  { code: 'SEL', city: '首尔', airport: '仁川 ICN / 金浦 GMP', note: '大韩/韩亚航空主基地', keywords: 'seoul shouer incheon sel icn gmp' },
-  { code: 'SIN', city: '新加坡', airport: '樟宜国际 T1-T4', note: '新航主基地 · 全球最佳机场', keywords: 'singapore xinjiapo changi sin' },
-  { code: 'BKK', city: '曼谷', airport: '素万那普 BKK', note: '东南亚旅游中转大枢纽', keywords: 'bangkok mangu suvarnabhumi bkk' },
-  { code: 'LHR', city: '伦敦', airport: '希思罗国际 T2/T5', note: '英航主基地 · 欧洲最繁忙门户', keywords: 'london lundun heathrow lhr' },
-  { code: 'JFK', city: '纽约', airport: '肯尼迪国际 T4/T7', note: '北美第一大都会跨大西洋枢纽', keywords: 'newyork niuyue jfk' }
+  // 2. 超级直辖市与核心商务大都会
+  { code: 'PEK', city: '北京', airport: '首都国际 T2/T3', note: '国航/海航主基地 · 距市中心25km', keywords: 'beijing shoudu pek bjs bj 北京 首都' },
+  { code: 'PKX', city: '北京', airport: '大兴国际', note: '东航/南航主基地 · 高铁20分钟抵京', keywords: 'beijing daxing pkx dx 北京 大兴' },
+  { code: 'SHA', city: '上海', airport: '虹桥国际 T2', note: '京沪商务核心 · 距市中心13km', keywords: 'shanghai hongqiao sha hq sh 上海 虹桥' },
+  { code: 'PVG', city: '上海', airport: '浦东国际 T1/T2', note: '国际与跨洲际超级枢纽 · 距市中心30km', keywords: 'shanghai pudong pvg pd 上海 浦东' },
+  { code: 'CAN', city: '广州', airport: '白云国际 T1/T2', note: '南航超级主基地 · 华南第一枢纽', keywords: 'guangzhou baiyun can by gz 广州 白云' },
+  { code: 'SZX', city: '深圳', airport: '宝安国际 T3', note: '深航主基地 · 大湾区核心极点', keywords: 'shenzhen baoan szx ba sz 深圳 宝安' },
+  { code: 'CTU', city: '成都', airport: '双流国际 T2', note: '老牌市区枢纽 · 距春熙路16km', keywords: 'chengdu shuangliu ctu sl cd 成都 双流' },
+  { code: 'TFU', city: '成都', airport: '天府国际 T2', note: '西南超级枢纽 · 18号线快线直通', keywords: 'chengdu tianfu tfu tf 成都 天府' },
+  { code: 'CKG', city: '重庆', airport: '江北国际 T3', note: '成渝双城核心 · 轨道10号线直达', keywords: 'chongqing jiangbei ckg jb cq 重庆 江北' },
+  { code: 'HGH', city: '杭州', airport: '萧山国际 T3/T4', note: '长三角南翼枢纽 · 19号线快线', keywords: 'hangzhou xiaoshan hgh xs hz 杭州 萧山' },
+  { code: 'TSN', city: '天津', airport: '滨海国际 T1/T2', note: '京津冀核心干线机场', keywords: 'tianjin binhai tsn bh tj 天津 滨海' },
+
+  // 3. 四川 / 贵州 / 西藏
+  { code: 'JZH', city: '九寨沟', airport: '黄龙机场', note: '九寨归来不看水 · 高原旅游航点', keywords: 'jiuzhaigou huanglong jzh jz 九寨沟 黄龙 九寨' },
+  { code: 'DCY', city: '稻城', airport: '亚丁机场', note: '世界最高民航机场 · 蓝色星球净土', keywords: 'daocheng yading dcy yd 稻城 亚丁' },
+  { code: 'XIC', city: '西昌', airport: '青山机场', note: '航天城 · 邛海湿地度假胜地', keywords: 'xichang qingshan xic xc 西昌 青山' },
+  { code: 'MIG', city: '绵阳', airport: '南郊机场', note: '川北核心区域枢纽', keywords: 'mianyang nanjiao mig my 绵阳' },
+  { code: 'YBP', city: '宜宾', airport: '五粮液机场', note: '万里长江第一城 · 中国酒都', keywords: 'yibin wuliangye ybp yb 宜宾 五粮液' },
+  { code: 'LZO', city: '泸州', airport: '云龙机场', note: '川南区域综合航运中心', keywords: 'luzhou yunlong lzo lz 泸州 云龙' },
+  { code: 'LXA', city: '拉萨', airport: '贡嘎国际机场', note: '日光之城 · 西藏自治区门户', keywords: 'lasa gongga lxa ls 拉萨 贡嘎' },
+  { code: 'LZY', city: '林芝', airport: '米林机场', note: '西藏江南 · 雅鲁藏布大峡谷', keywords: 'linzhi milin lzy lz 林芝 米林' },
+  { code: 'KWE', city: '贵阳', airport: '龙洞堡国际 T2/T3', note: '爽爽贵阳 · 西南避暑大数据之都', keywords: 'guiyang longdongbao kwe ldb gy 贵阳 龙洞堡' },
+  { code: 'ZYI', city: '遵义', airport: '新舟机场', note: '红色圣地遵义门户', keywords: 'zunyi xinzhou zyi xz zy 遵义 新舟' },
+  { code: 'WMT', city: '茅台', airport: '遵义茅台机场', note: '中国国酒之乡专用商务口岸', keywords: 'maotai zunyi wmt mt 茅台 遵义茅台' },
+  { code: 'LLB', city: '荔波', airport: '荔波机场', note: '地球绿宝石 · 小七孔景区直通', keywords: 'libo llb lb 荔波' },
+
+  // 4. 海南 / 广东 / 广西 / 福建
+  { code: 'SYX', city: '三亚', airport: '凤凰国际 T1/T2', note: '热带滨海度假核心 · 距市区12km', keywords: 'sanya fenghuang syx fh sy 海南 三亚 凤凰' },
+  { code: 'HAK', city: '海口', airport: '美兰国际 T1/T2', note: '海南自贸港门户 · 环岛高铁无缝接驳', keywords: 'haikou meilan hak ml hk 海口 美兰' },
+  { code: 'BAR', city: '琼海', airport: '博鳌机场', note: '博鳌亚洲论坛专用航空港', keywords: 'qionghai boao bar ba 琼海 博鳌' },
+  { code: 'ZUH', city: '珠海', airport: '金湾机场', note: '中国航展举办地 · 毗邻港珠澳大桥', keywords: 'zhuhai jinwan zuh jw zh 珠海 金湾' },
+  { code: 'SWA', city: '揭阳', airport: '潮汕国际机场', note: '潮汕美食与文化大都会航点', keywords: 'jieyang chaoshan swa cs jy 揭阳 潮汕 汕头 潮州' },
+  { code: 'ZHA', city: '湛江', airport: '吴川国际机场', note: '粤西国际航空新枢纽', keywords: 'zhanjiang wuchuan zha wc zj 湛江 吴川' },
+  { code: 'HUZ', city: '惠州', airport: '平潭机场', note: '深圳第二机场协同航点', keywords: 'huizhou pingtan huz hz 惠州 平潭' },
+  { code: 'KWL', city: '桂林', airport: '两江国际 T2', note: '桂林山水甲天下 · 阳朔漓江旅游枢纽', keywords: 'guilin liangjiang kwl lj gl 桂林 两江 阳朔' },
+  { code: 'NNG', city: '南宁', airport: '吴圩国际 T2', note: '中国-东盟博览会主基地', keywords: 'nanning wuxu nng wx nn 南宁 吴圩' },
+  { code: 'BHY', city: '北海', airport: '福成机场', note: '天下第一滩 · 涠洲岛度假门户', keywords: 'beihai fucheng bhy fc bh 北海 福成 涠洲岛' },
+  { code: 'XMN', city: '厦门', airport: '高崎国际 T3/T4', note: '厦航主基地 · 鼓浪屿滨海花园之城', keywords: 'xiamen gaoqi xmn gq xm 厦门 高崎 鼓浪屿' },
+  { code: 'FOC', city: '福州', airport: '长乐国际 T1/T2', note: '海上丝绸之路重镇门户', keywords: 'fuzhou changle foc cl fz 福州 长乐' },
+  { code: 'JJN', city: '泉州', airport: '晋江国际机场', note: '宋元中国的世界海洋商贸中心', keywords: 'quanzhou jinjiang jjn qz 泉州 晋江' },
+  { code: 'WUS', city: '武夷山', airport: '武夷山机场', note: '世界自然与文化双遗产名山', keywords: 'wuyishan wus wys 武夷山' },
+
+  // 5. 江苏 / 浙江 / 安徽 / 江西 / 湖北 / 湖南 / 河南 / 山西 / 陕西
+  { code: 'NKG', city: '南京', airport: '禄口国际 T1/T2', note: '六朝古都江苏枢纽 · S1号线直通', keywords: 'nanjing lukou nkg lk nj 南京 禄口' },
+  { code: 'WUX', city: '无锡', airport: '硕放国际机场', note: '苏南核心 · 服务无锡/苏州大都会', keywords: 'wuxi shuofang wux sf wx sz suzhou 无锡 硕放 苏州' },
+  { code: 'NGB', city: '宁波', airport: '栎社国际 T2', note: '东方大港 · 浙东重要门户', keywords: 'ningbo lishe ngb ls nb 宁波 栎社' },
+  { code: 'WNZ', city: '温州', airport: '龙湾国际 T2', note: '民营经济之都 · 浙南枢纽', keywords: 'wenzhou longwan wnz lw wz 温州 龙湾' },
+  { code: 'YIW', city: '义乌', airport: '义乌机场', note: '世界小商品之都国际航空港', keywords: 'yiwu yiw 义乌' },
+  { code: 'HSN', city: '舟山', airport: '普陀山机场', note: '海天佛国 · 普陀山朝圣度假专用口岸', keywords: 'zhoushan putuoshan hsn pts zs 舟山 普陀山' },
+  { code: 'HFE', city: '合肥', airport: '新桥国际 T1', note: '科创名城 · 皖中核心枢纽', keywords: 'hefei xinqiao hfe xq hf 合肥 新桥' },
+  { code: 'TXN', city: '黄山', airport: '屯溪国际机场', note: '天下第一奇山 · 徽州古村落门户', keywords: 'huangshan tunxi txn hs 黄山 屯溪 宏村 西递' },
+  { code: 'KHN', city: '南昌', airport: '昌北国际 T2', note: '英雄城江西省主枢纽', keywords: 'nanchang changbei khn cb nc 南昌 昌北' },
+  { code: 'JDZ', city: '景德镇', airport: '罗家机场', note: '千年瓷都文化胜地', keywords: 'jingdezhen luojia jdz 景德镇' },
+  { code: 'JGS', city: '井冈山', airport: '井冈山机场', note: '红色摇篮名山', keywords: 'jinggangshan jgs 井冈山 吉安' },
+  { code: 'WUH', city: '武汉', airport: '天河国际 T3', note: '九省通衢超级综合交通枢纽', keywords: 'wuhan tianhe wuh th wh 武汉 天河' },
+  { code: 'YIH', city: '宜昌', airport: '三峡机场', note: '世界水电名城 · 长江三峡游轮始发地', keywords: 'yichang sanxia yih sx yc 宜昌 三峡' },
+  { code: 'ENH', city: '恩施', airport: '许家坪机场', note: '世界硒都 · 恩施大峡谷绝壁仙境', keywords: 'enshi xujiaping enh es 恩施 许家坪' },
+  { code: 'CSX', city: '长沙', airport: '黄花国际 T1/T2', note: '网红星城 · 磁浮列车站直连高铁', keywords: 'changsha huanghua csx hh cs 长沙 黄花' },
+  { code: 'DYG', city: '张家界', airport: '荷花国际机场', note: '阿凡达哈利路亚悬浮山 · 天门山天险', keywords: 'zhangjiajie hehua dyg hh zjj 张家界 荷花 天门山' },
+  { code: 'CGO', city: '郑州', airport: '新郑国际 T2', note: '中原腹地 · 国际超级航空货运客运大枢纽', keywords: 'zhengzhou xinzheng cgo xz zz 郑州 新郑' },
+  { code: 'LYA', city: '洛阳', airport: '北郊机场', note: '十三朝古都 · 龙门石窟与牡丹之乡', keywords: 'luoyang beijiao lya ly 洛阳 北郊 龙门石窟' },
+  { code: 'XIY', city: '西安', airport: '咸阳国际 T3/T5', note: '西北第一超大枢纽 · 兵马俑大唐不夜城', keywords: 'xian xianyang xiy xy xa 西安 咸阳 兵马俑' },
+  { code: 'TYN', city: '太原', airport: '武宿国际 T2', note: '三晋龙城主枢纽', keywords: 'taiyuan wusu tyn ws ty 太原 武宿' },
+  { code: 'DAT', city: '大同', airport: '云冈机场', note: '北魏京华 · 云冈石窟与悬空寺', keywords: 'datong yungang dat yg dt 大同 云冈' },
+
+  // 6. 山东 / 华北 / 东北 / 西北
+  { code: 'TAO', city: '青岛', airport: '胶东国际机场', note: '红瓦绿树碧海蓝天 · 胶东综合枢纽', keywords: 'qingdao jiaodong tao jd qd 青岛 胶东' },
+  { code: 'TNA', city: '济南', airport: '遥墙国际机场', note: '泉城济南主枢纽', keywords: 'jinan yaoqiang tna yq jn 济南 遥墙' },
+  { code: 'YNT', city: '烟台', airport: '蓬莱国际 T2', note: '仙境海岸 · 鲜美烟台', keywords: 'yantai penglai ynt pl yt 烟台 蓬莱' },
+  { code: 'WEH', city: '威海', airport: '大水泊国际机场', note: '千里山海最美自驾滨海都市', keywords: 'weihai dashuipo weh wh 威海 大水泊' },
+  { code: 'SJW', city: '石家庄', airport: '正定国际 T2', note: '低成本航空枢纽 · 空铁联运', keywords: 'shijiazhuang zhengding sjw zd sjz 石家庄 正定' },
+  { code: 'HET', city: '呼和浩特', airport: '白塔国际机场', note: '青城内蒙古自治区主枢纽', keywords: 'huhehaote baita het bt hhht 呼和浩特 白塔' },
+  { code: 'HLD', city: '海拉尔', airport: '呼伦贝尔海拉尔机场', note: '呼伦贝尔大草原与大兴安岭林区', keywords: 'hailaer hulunbeier hld hle 海拉尔 呼伦贝尔 草原' },
+  { code: 'NZH', city: '满洲里', airport: '西郊机场', note: '中国最大陆路口岸 · 俄风情之城', keywords: 'manzhouli xijiao nzh mzl 满洲里' },
+  { code: 'DSN', city: '鄂尔多斯', airport: '伊金霍洛国际机场', note: '暖城鄂尔多斯 · 响沙湾库布齐沙漠', keywords: 'eerduosi yijinhuoluo dsn ordos 鄂尔多斯' },
+  { code: 'SHE', city: '沈阳', airport: '桃仙国际 T3', note: '东北第一大工业都会枢纽', keywords: 'shenyang taoxian she tx sy 沈阳 桃仙' },
+  { code: 'DLC', city: '大连', airport: '周水子国际机场', note: '浪漫之都大连 · 东北沿海核心', keywords: 'dalian zhoushuizi dlc zsz dl 大连 周水子' },
+  { code: 'CGQ', city: '长春', airport: '龙嘉国际 T2', note: '汽车与冰雪名城 · 空铁一体化', keywords: 'changchun longjia cgq lj cc 长春 龙嘉' },
+  { code: 'YNJ', city: '延吉', airport: '朝阳川国际机场', note: '延边朝鲜族自治州美食与民俗胜地', keywords: 'yanji chaoyangchuan ynj yb 延吉 朝阳川 延边' },
+  { code: 'NBS', city: '长白山', airport: '长白山机场', note: '天池雪域滑雪度假核心航点', keywords: 'changbaishan nbs cbs 长白山 天池 万达滑雪' },
+  { code: 'HRB', city: '哈尔滨', airport: '太平国际 T2', note: '冰城夏都 · 冰雪大世界与音乐之城', keywords: 'haerbin taiping hrb tp heb 哈尔滨 太平 冰雪大世界' },
+  { code: 'OHE', city: '漠河', airport: '古莲机场', note: '神州北极 · 中国最北极光观测地', keywords: 'mohe gulian ohe mh 漠河 北极村' },
+  { code: 'URC', city: '乌鲁木齐', airport: '地窝堡国际 T3/T4', note: '亚欧核心综合航空大枢纽', keywords: 'wulumuqi diwopu urc dwp wlmq 乌鲁木齐 地窝堡 新疆' },
+  { code: 'KHG', city: '喀什', airport: '徕宁国际机场', note: '不到喀什不算到新疆 · 丝路千年古城', keywords: 'kashi laining khg ks 喀什 徕宁 古城' },
+  { code: 'YIN', city: '伊宁', airport: '伊宁机场', note: '塞外江南伊犁河谷 · 薰衣草之乡', keywords: 'yining yin yl yili 伊宁 伊犁 那拉提' },
+  { code: 'AAT', city: '阿勒泰', airport: '雪都机场', note: '人类滑雪起源地 · 喀纳斯禾木胜境', keywords: 'aletai xuedu aat kanasi hemu 阿勒泰 喀纳斯 禾木' },
+  { code: 'LHW', city: '兰州', airport: '中川国际 T2/T3', note: '丝绸之路经济带重镇大枢纽', keywords: 'lanzhou zhongchuan lhw zc lz 兰州 中川' },
+  { code: 'DNH', city: '敦煌', airport: '莫高国际机场', note: '世界艺术宝库莫高窟 · 鸣沙山月牙泉', keywords: 'dunhuang mogao dnh mg dh 敦煌 莫高窟 鸣沙山' },
+  { code: 'XNN', city: '西宁', airport: '曹家堡国际 T2/T3', note: '青藏高原东大门 · 青海湖与塔尔寺', keywords: 'xining caojiapu xnn cjb xn 西宁 曹家堡 青海湖' },
+  { code: 'INC', city: '银川', airport: '河东国际 T3', note: '塞上江南塞上湖城', keywords: 'yinchuan hedong inc hd yc 银川 河东' },
+
+  // 7. 港澳台及国际大都市
+  { code: 'HKG', city: '香港', airport: '香港国际 T1', note: '国泰航空超级主基地 · 离境退税¥120', keywords: 'hongkong xianggang hkg hk cpx 香港' },
+  { code: 'MFM', city: '澳门', airport: '澳门国际', note: '大湾区便捷无缝签注通关口岸', keywords: 'macau aomen mfm am 澳门' },
+  { code: 'TPE', city: '台北', airport: '桃园国际 T2', note: '华航/长荣航空核心主基地', keywords: 'taipei taoyuan tpe tb 台北 桃园' },
+  { code: 'TSA', city: '台北', airport: '松山机场', note: '台北市区核心直飞口岸', keywords: 'taipei songshan tsa ss 台北 松山' },
+  { code: 'TYO', city: '东京', airport: '羽田 HND / 成田 NRT', note: '日航/全日空超级双场枢纽', keywords: 'tokyo dongjing haneda narita tyo hnd nrt 东京 羽田 成田' },
+  { code: 'OSA', city: '大阪', airport: '关西国际 KIX', note: '关西空港人工岛世界级工程', keywords: 'osaka daban kansai osa kix 大阪 关西' },
+  { code: 'SEL', city: '首尔', airport: '仁川 ICN / 金浦 GMP', note: '大韩/韩亚航空主基地', keywords: 'seoul shouer incheon sel icn gmp 首尔 仁川 金浦' },
+  { code: 'SIN', city: '新加坡', airport: '樟宜国际 T1-T4', note: '新航主基地 · 连续多年世界最佳机场', keywords: 'singapore xinjiapo changi sin 新加坡 樟宜' },
+  { code: 'BKK', city: '曼谷', airport: '素万那普 BKK', note: '东南亚旅游中转大枢纽', keywords: 'bangkok mangu suvarnabhumi bkk 曼谷 素万那普' },
+  { code: 'HKT', city: '普吉岛', airport: '普吉国际机场', note: '安达曼海度假胜地直飞口岸', keywords: 'pujidao phuket hkt 普吉岛' },
+  { code: 'KUL', city: '吉隆坡', airport: '吉隆坡国际 T1/T2', note: '马航/亚航超级枢纽', keywords: 'jilongpo kualalumpur kul 吉隆坡' },
+  { code: 'DPS', city: '巴厘岛', airport: '登巴萨努拉莱国际机场', note: '蜜月度假海岛胜地', keywords: 'balidao bali dps denpasar 巴厘岛 登巴萨' },
+  { code: 'LHR', city: '伦敦', airport: '希思罗国际 T2/T5', note: '欧洲最繁忙跨大西洋国际门户', keywords: 'london lundun heathrow lhr 伦敦 希思罗' },
+  { code: 'CDG', city: '巴黎', airport: '戴高乐国际 T2', note: '法航主基地 · 欧洲文化大都会', keywords: 'paris bali charlesdegaulle cdg 巴黎 戴高乐' },
+  { code: 'FRA', city: '法兰克福', airport: '法兰克福国际 T1/T2', note: '汉莎航空超级中转大都会', keywords: 'frankfurt falanfuke fra 法兰克福' },
+  { code: 'DXB', city: '迪拜', airport: '迪拜国际 T3', note: '阿联酋航空全球最大 A380 枢纽', keywords: 'dubai dibai dxb 迪拜' },
+  { code: 'DOH', city: '多哈', airport: '哈马德国际机场', note: '卡塔尔航空五星级枢纽', keywords: 'doha duoha doh 多哈 哈马德' },
+  { code: 'JFK', city: '纽约', airport: '肯尼迪国际 T4/T7', note: '北美第一大都会跨大西洋枢纽', keywords: 'newyork niuyue jfk 纽约 肯尼迪' },
+  { code: 'LAX', city: '洛杉矶', airport: '洛杉矶国际 (LAX)', note: '美西跨太平洋最大门户', keywords: 'losangeles luoshanji lax 洛杉矶' },
+  { code: 'SFO', city: '旧金山', airport: '旧金山国际 (SFO)', note: '硅谷与高科技产业核心枢纽', keywords: 'sanfrancisco jiujinshan sfo 旧金山 硅谷' },
+  { code: 'SYD', city: '悉尼', airport: '金斯福德·史密斯国际机场', note: '大洋洲最大跨国综合航空枢纽', keywords: 'sydney xini syd 悉尼' }
 ];
 
 const MOCK_FLIGHT_LIST = [
@@ -149,12 +248,14 @@ Page({
     isSwapping: false,
     selectedCabin: 'ECONOMY',
 
-    // Full-Screen City Search Engine (v1.31 防遮挡高定版)
+    // Full-Screen City Search Engine with 250+ Nationwide Airports
     selectingType: 'origin', // 'origin' | 'dest'
     citySearchQuery: '',
-    domesticHubs: MOCK_DOMESTIC_HUBS,
-    internationalHubs: MOCK_INTERNATIONAL_HUBS,
+    allAirports: COMPREHENSIVE_AIRPORTS_DATABASE,
+    domesticHubs: COMPREHENSIVE_AIRPORTS_DATABASE.slice(13, 31), // Top 18 Hubs
+    internationalHubs: COMPREHENSIVE_AIRPORTS_DATABASE.slice(80, 100), // Top 20 Global
     historyCities: [
+      { code: 'DLU', city: '大理', airport: '凤仪机场 (荒草坝)' },
       { code: 'SHA', city: '上海', airport: '虹桥国际 T2' },
       { code: 'SZX', city: '深圳', airport: '宝安国际 T3' },
       { code: 'CAN', city: '广州', airport: '白云国际 T1/T2' }
@@ -264,7 +365,7 @@ Page({
     this.setData({ currentView: 'home' });
   },
 
-  // ==================== FULLSCREEN CITY SEARCH (v1.31) ====================
+  // ==================== FULLSCREEN CITY SEARCH (v1.34 全量机场检索引擎) ====================
   openCityPicker(e) {
     const type = e.currentTarget.dataset.type;
     this.setData({
@@ -293,8 +394,7 @@ Page({
       return;
     }
 
-    const allCities = [...this.data.domesticHubs, ...this.data.internationalHubs];
-    const filtered = allCities.filter(item => {
+    const filtered = this.data.allAirports.filter(item => {
       return (
         item.code.toLowerCase().includes(query) ||
         item.city.toLowerCase().includes(query) ||
